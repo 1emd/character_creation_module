ed0df9b7cc28aacbe491d05d3f3e4ce764c93bc1
Кирилл Хорошилов <khoroshilovkirill@gmail.com>
2022-12-17 20:02:11 +0300

    Homework rework

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """При вызове функции происходит атака героя."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон'
                 f'противнику равный {5 + randint(3, 5)}')
@@ -15,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """При вызове функции персонаж блокирует урон."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """При вызове функции применяется специальное умение."""
     if char_class == 'warrior':
         return (f'{char_name:} применил'
                 f'специальное умение «Выносливость {80 + 25}»')
@@ -38,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Данная функция обучает командам."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -63,6 +69,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция отвечает за выбор класса персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -84,7 +91,9 @@ def choice_char_class() -> str:
     return str(char_class)
 
 
-def main():
+if __name__ == '__main__':
+    """Приветсвие и выбор имени героя."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -96,4 +105,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+# main()
